42241493448b8877727bf9322ab979fd023835b2
Arthur Lockman <arthurlockman@ajobi.net>
2012-09-25 17:39:31 -0400

    Week 3
    Added Week 3 project.

---
+++ Week3 Project/src/SystemOutDemo.java
@@ -0,0 +1,21 @@
+/**
+ * Author: Arthur Lockman
+ * School: Gorham High School
+ * Project: System.out plug and play
+ * Date: September 19, 2012
+ * 
+ * @author Arthur Lockman
+ * @version 1.0
+ */
+public class SystemOutDemo 
+{
+	/**
+	 * @brief The main loop of the program.
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Java is pretty cool. Hello, world!");
+	}
+
+}
